10a1bb705f41a264fb676f7c63b829f67541f2fb
Mazin Karjikar <mazkar3@gmail.com>
2025-03-28 01:21:06 -0700

    added factorization using sieve's primes

--- cpp/primes_to.cpp
+++ cpp/primes_to.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// SIEVE OF ERASTOTHENES
 
 int PRIME_GEN_MAX = 2 * 1e5 + 10;
 
@@ -9,7 +10,7 @@ vector<bool> is_prime(PRIME_GEN_MAX + 1, true);
 
 int main() {
 
-// --------- Copy this into main----------
+// --------- Copy this into main--------------------------------------
 
     is_prime[0] = is_prime[1] = false;
     for(int i = 2; i * i <= PRIME_GEN_MAX; i++) {
@@ -19,10 +20,10 @@ int main() {
             }
         }
     }
-    // optional to get list of primes
+    // optional to get list of primes. for factorization using primes, only compute until sqrt(max(array)), for is_prime as well.
     for(int i = 2; i <= PRIME_GEN_MAX; i++) if (is_prime[i]) primes.push_back(i);
 
-// -------------- Done --------------------
+// -------------- Done ------------------------------------------------
 
     for(auto x: primes) cout << x << " ";
 
