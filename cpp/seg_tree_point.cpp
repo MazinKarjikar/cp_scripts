08429f7807f61eba748593bf33ff00753fc82cdd
Mazin Karjikar <mazkar3@gmail.com>
2025-11-19 23:48:27 -0500

    added segtree with point updates. will add lazy prop later

--- cpp/seg_tree_point.cpp
+++ cpp/seg_tree_point.cpp
@@ -3,73 +3,73 @@ using namespace std;
 /** A data structure that can answer point update & range min queries. */
 
 template <class T> class SegmentTree {
+public:
 
-    private:
+    SegmentTree(const vector<T> &arr, const T def) : len(arr.size()), DEFAULT(def) {
+        segtree = vector<T>(len * 4, DEFAULT);
+        build(arr, 1, 0, len - 1);
+    }
 
-        const T DEFAULT = std::numeric_limits<T>().max();
-        int len;
-        vector<T> segtree;  // index 0 is not in use
+    /** Sets the value at ind to val. */
+    void set(int ind, T val) { set(ind, val, 1, 0, len - 1); }
 
-        T combine(const T &a, const T &b) { return std::min(a, b); }
+    /** @return the minimum element in the range [start, end] */
+    T range_query(int start, int end) { return range_query(start, end, 1, 0, len - 1); }
 
-        void build(const vector<T> &arr, int at, int at_left, int at_right) {
-            if (at_left == at_right) {
-                segtree[at] = arr[at_left];
-                return;
-            }
+private:
 
-            int mid = (at_left + at_right) / 2;
+    int len;
+    const T DEFAULT;
+    vector<T> segtree;  // index 0 is not in use
 
-            build(arr, 2 * at, at_left, mid);
-            build(arr, 2 * at + 1, mid + 1, at_right);
+    T combine(const T &a, const T &b) { return min(a, b); }
 
-            segtree[at] = combine(segtree[2 * at], segtree[2 * at + 1]);
+    void build(const vector<T> &arr, int at, int at_left, int at_right) {
+        if (at_left == at_right) {
+            segtree[at] = arr[at_left];
+            return;
         }
 
-        void set(int ind, T val, int at, int at_left, int at_right) {
+        int mid = (at_left + at_right) / 2;
 
-            if (at_left == at_right) {
-                segtree[at] = val;
-                return;
-            }
+        build(arr, 2 * at, at_left, mid);
+        build(arr, 2 * at + 1, mid + 1, at_right);
 
-            int mid = (at_left + at_right) / 2;
+        segtree[at] = combine(segtree[2 * at], segtree[2 * at + 1]);
+    }
 
-            if (ind <= mid) {
-                set(ind, val, 2 * at, at_left, mid);
-            } else {
-                set(ind, val, 2 * at + 1, mid + 1, at_right);
-            }
+    void set(int ind, T val, int at, int at_left, int at_right) {
 
-            segtree[at] = combine(segtree[2 * at], segtree[2 * at + 1]);
+        if (at_left == at_right) {
+            segtree[at] = val;
+            return;
         }
 
+        int mid = (at_left + at_right) / 2;
 
-        T range_query(int start, int end, int at, int at_left, int at_right) {
-
-            if (at_right < start || end < at_left) { return DEFAULT; }
-            if (start <= at_left && at_right <= end) { return segtree[at]; }
-
-            int mid = (at_left + at_right) / 2;
-
-            T left_res = range_query(start, end, 2 * at, at_left, mid);
-            T right_res = range_query(start, end, 2 * at + 1, mid + 1, at_right);
-
-            return combine(left_res, right_res);
+        if (ind <= mid) {
+            set(ind, val, 2 * at, at_left, mid);
+        } else {
+            set(ind, val, 2 * at + 1, mid + 1, at_right);
         }
 
-    public:
+        segtree[at] = combine(segtree[2 * at], segtree[2 * at + 1]);
+    }
 
-        SegmentTree(const vector<T> &arr, const T def) : len(arr.size()), DEFAULT(def) {
-            segtree = vector<T>(len * 4, DEFAULT);
-            build(arr, 1, 0, len - 1);
-        }
 
-        /** Sets the value at ind to val. */
-        void set(int ind, T val) { set(ind, val, 1, 0, len - 1); }
+    T range_query(int start, int end, int at, int at_left, int at_right) {
 
-        /** @return the minimum element in the range [start, end] */
-        T range_query(int start, int end) { return range_query(start, end, 1, 0, len - 1); }
+        if (at_right < start || end < at_left) { return DEFAULT; }
+        if (start <= at_left && at_right <= end) { return segtree[at]; }
+
+        int mid = (at_left + at_right) / 2;
+
+        T left_res = range_query(start, end, 2 * at, at_left, mid);
+        T right_res = range_query(start, end, 2 * at + 1, mid + 1, at_right);
+
+        return combine(left_res, right_res);
+    }
+
 };
 
 int main() {
@@ -78,7 +78,7 @@ int main() {
 
     SegmentTree<int> st(a, 100);
 
-    int ans = st.range_query(3, 4);
+    int ans = st.range_query(3, 3);
     cout << ans << endl;
 
 };
